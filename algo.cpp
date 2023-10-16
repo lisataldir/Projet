53a348b6ce0d3b4500f748e215f20e366af1508f
Lisa Taldir <145778985+lisataldir@users.noreply.github.com>
2023-10-16 09:40:58 +0200

    Add files via upload
    Initialisation du projet sur l'arrondi stochastique pour le calcul scientifique

---
+++ algo.cpp
@@ -0,0 +1,13 @@
+#include "algo.h"
+
+double exp_naif(double x, int n){
+    return 0;
+}
+
+double exp(double x, int n){
+    return 0;
+}
+
+int main(){
+    return 0;
+}
---
+++ algo.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+double exp_naif(double x, int n);
+
+double exp(double x, int n);
