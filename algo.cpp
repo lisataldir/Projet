b33a88fa110ced0bc34806cfecca76a84a473b1a
“Yutai <yutai.zhao@gmail.com>
2023-10-16 12:38:46 +0200

    1ere Implementation Exponentation
    -Draft: les deux methodes (naive et rapide) sont implementees
      -> pas sur si c'est ce qui est demande
      -> print le resultat de 2^3 = 8 dans la fonction main
    -compilation avec clang algo.cpp
    -sinon le fichier executable a.out est fourni

--- algo.cpp
+++ algo.cpp
@@ -1,13 +1,34 @@
 #include "algo.h"
+#include <stdlib.h>
 
-double exp_naif(double x, int n){
-    return 0;
+double exp_naif(double const x, int n){
+    double res = 1 ;
+    for (int i=0;i<n;++i){
+        res = res*x;
+    }
+    return res;
 }
 
-double exp(double x, int n){
-    return 0;
+double exp_rapide(double const x, int n){
+    if(n==0){return 0;}
+    double res = 1 ;
+    int r = n%2;
+    int p = n/2;
+    if (r==0) {
+        res=exp_naif(x,p)*exp_naif(x,p);
+    }
+    if (r==1) {
+        res=exp_naif(x,p)*exp_naif(x,p)*x;
+    }
+    return res;
 }
 
 int main(){
+    double x = 2;
+    int n = 3;
+    double r1 = exp_naif( x,  n);
+    double r2 = exp_rapide( x,  n);
+    printf("exp_naif de %f^%d est : %f \n", x,n,r1);
+    printf("exp_rapide de %f^%d est : %f \n", x,n,r2);
     return 0;
 }
