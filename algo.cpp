859fc02e61cec1efe7490d9b7b0ef7d099ac95fe
“Yutai <yutai.zhao@gmail.com>
2023-10-20 03:30:43 +0200

    1st try with SR and algo mistakes solved
    - Ajout de 3 nouveaux *.txt
     -> les resultats sont enregistres sous forme :
    	1ere col : algo naif
    	2eme col : algo rapide
        NB : les 2 cols sont separees par un espace
     -> res_ieee.txt contient le resultat de 2.7^30 avec ieee
     -> res.txt contient les resultats de 2.7^30 avec SR
     -> commande.txt contient les commandes utilisees pour verificarlo
    
    - Modification de algo.cpp
     -> x est une base double, sinon la version precedente est limitee a int
     -> n est son exposant int, sinon la version precedente est limitee a int>0
     -> Besoin de 3 args mtn, le 3eme est un bool :
    	true : afficher les resultats avec temps pris
    	false : afficher que les resultats

--- algo.cpp
+++ algo.cpp
@@ -1,18 +1,27 @@
 #include "algo.h"
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 double exp_naif(double const x, int n){
-    double res = 1 ;
-    for (int i=0;i<n;++i){
-        res = res*x;
+    double res = 1.0 ;
+    if (n>0){
+        for (int i=0;i<n;++i){
+            res = res*x;
+        }
+    }
+    if(n<0){
+        n=-1*n;
+        for (int i=0;i<n;++i){
+            res = res*(1/x);
+        }
     }
     return res;
 }
 
 double exp_rapide(double const x, int n){
     
-    double res = 1 ; //initialisation du resultat
+    double res = 1.0 ; //initialisation du resultat
     
     if(n==0){return 1;}
     
@@ -23,38 +32,43 @@ double exp_rapide(double const x, int n){
     if (r==0) {//Oui, ie: x^n = x^2p
         res=exp_naif(x,p)*exp_naif(x,p);
         //on divise le calcul initial en 2 calculs parallels
-    }
-    
-    if (r==1) {//Non, ie: x^n = x^2p*x
-        res=exp_naif(x,p)*exp_naif(x,p)*x;
+    } else {//Non, ie: x^n = x^2p*x
+        res=exp_naif(x,p)*exp_naif(x,p)*(1/x);
         //analogiquement
     }
     return res;
 }
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
     
-    double x = strtol(argv[1], NULL, 10);
-    int n = strtol(argv[2], NULL, 10);
-    
-    clock_t begin_r1 = clock();
-    double r1 = exp_naif( x,  n);
-    clock_t end_r1 = clock();
-    
-    clock_t begin_r2 = clock();
-    double r2 = exp_rapide( x,  n);
-    clock_t end_r2 = clock();
-    
-    unsigned long millis1 = (end_r1 -  begin_r1) * 1000 / CLOCKS_PER_SEC;
-    unsigned long millis2 = (end_r2 -  begin_r2) * 1000 / CLOCKS_PER_SEC;
-    
-    
-    printf("exp_naif de %f^%d est : %f \n", x,n,r1);
-    printf( "exp_naif done in %ld ms\n", millis1 );
-    
-    printf("exp_rapide de %f^%d est : %f \n", x,n,r2);
-    printf( "exp_rapide done in %ld ms\n", millis2 );
-    
+    double x = atof(argv[1]);
+    int n = atoi(argv[2]);
+    bool check_time = atoi(argv[3]);
+        
+    if(check_time){
+        clock_t begin_r1 = clock();
+        double r1 = exp_naif( x,  n);
+        clock_t end_r1 = clock();
+        
+        clock_t begin_r2 = clock();
+        double r2 = exp_rapide( x,  n);
+        clock_t end_r2 = clock();
+        
+        unsigned long millis1 = (end_r1 -  begin_r1) * 1000 / CLOCKS_PER_SEC;
+        unsigned long millis2 = (end_r2 -  begin_r2) * 1000 / CLOCKS_PER_SEC;
+        
+        printf( "==========================\n");
+        printf("exp_naif de %f^%d est : %f \n", x,n,r1);
+        printf( "exp_naif done in %ld ms\n", millis1 );
+        
+        printf("exp_rapide de %f^%d est : %f \n", x,n,r2);
+        printf( "exp_rapide done in %ld ms\n", millis2 );
+        printf( "==========================\n");
+    }else{
+        double r1 = exp_rapide( x,  n);
+        double r2 = exp_rapide( x,  n);
+        printf("%f;%f\n", r1,r2);
+    }
     
     return 0;
 }
