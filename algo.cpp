0b59baeadaa69e9c0d50eeca57e71db7e7bf312d
“Yutai <yutai.zhao@gmail.com>
2023-10-26 04:22:14 +0200

    exp functions modification
    -exp_naif_recursif
     ->utilisant une methode recurssive
    -exp_rapide_iteratif
     ->utilisant une iteration et manipulation des bits
     ->draft, prob principal : sens de lecture inverse de l'expression binaire
     ->a priori c'est resout.....mais pas sure, tested simply

--- algo.cpp
+++ algo.cpp
@@ -3,72 +3,43 @@
 #include <time.h>
 #include <math.h>
 
-double exp_naif(double const x, int n){
-    double res = 1.0 ;
-    if (n>0){
-        for (int i=0;i<n;++i){
-            res = res*x;
-        }
+double exp_naif_recursif(double const x, int n){
+    if (n==0){
+        return 1;
+    }else{
+        return x*exp_naif_recursif(x, n-1);
     }
-    if(n<0){
-        n=-1*n;
-        for (int i=0;i<n;++i){
-            res = res*(1/x);
-        }
-    }
-    return res;
 }
 
-double exp_rapide(double const x, int n){
+double exp_rapide_iteratif(double const x, int n){
+    int n_init = n;
+    int cnt = 0;
+    double res = 1;
+    if (n==0) {return 1;}
     
-    double res = 1.0 ; //initialisation du resultat
-    
-    if(n==0){return 1;}
-    
-    int r = n%2;
-    int p = n/2;
-    
-    //verifions si l'exposant est divisbile par deux
-    if (r==0) {//Oui, ie: x^n = x^2p
-        res=exp_naif(x,p)*exp_naif(x,p);
-        //on divise le calcul initial en 2 calculs parallels
-    } else {//Non, ie: x^n = x^2p*x
-        res=exp_naif(x,p)*exp_naif(x,p)*(1/x);
-        //analogiquement
+        
+    while (n){
+        printf("%d \n",n );
+        res =res*res;
+        printf(" n = %d, cnt =%d , n & 1 = %d, (n>>1 &1) = %d, (n_init<<cnt &1) =%d \n",n,cnt, n & 1 , n>>1 &1, n_init<<cnt &1);
+        if ((n & 1) != 1 || (n>>1 &1) != 1 || (n_init<<cnt & 1) != 0){
+            printf(" n = %d, get in \n",n );
+            res =x*res;
+        }
+        n = n>>1;
+        ++cnt;
+        printf("res = %f \n", res );
     }
     return res;
 }
 
 int main(int argc, char *argv[]){
-    
-    double x = atof(argv[1]);
-    int n = atoi(argv[2]);
-    bool check_time = atoi(argv[3]);
-        
-    if(check_time){
-        clock_t begin_r1 = clock();
-        double r1 = exp_naif( x,  n);
-        clock_t end_r1 = clock();
-        
-        clock_t begin_r2 = clock();
-        double r2 = exp_rapide( x,  n);
-        clock_t end_r2 = clock();
-        
-        unsigned long millis1 = (end_r1 -  begin_r1) * 1000 / CLOCKS_PER_SEC;
-        unsigned long millis2 = (end_r2 -  begin_r2) * 1000 / CLOCKS_PER_SEC;
-        
-        printf( "==========================\n");
-        printf("exp_naif de %f^%d est : %f \n", x,n,r1);
-        printf( "exp_naif done in %ld ms\n", millis1 );
-        
-        printf("exp_rapide de %f^%d est : %f \n", x,n,r2);
-        printf( "exp_rapide done in %ld ms\n", millis2 );
-        printf( "==========================\n");
-    }else{
-        double r1 = exp_rapide( x,  n);
-        double r2 = exp_rapide( x,  n);
-        printf("%f;%f\n", r1,r2);
-    }
-    
+    double x= 2;
+    int n = 13;
+    double r = exp_rapide_iteratif( x, n);
+    double rr = exp_rapide_iteratif( 2, 3);
+    double rrr = exp_naif_recursif( 2, 13);
+    printf("rrr = %f \n", rrr );
+    printf("r = %f \n", r );
     return 0;
 }
