cdb7215cb70eb53c7600c8d7bc4ce141b7b4fed5
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2023-11-24 06:05:00 +0100

    rapid recursive method added

--- algo.cpp
+++ algo.cpp
@@ -10,9 +10,24 @@ double exp_naif_rec(double x, int n){
 double exp_naif_iter(double x, int n){
     return 0;
 }
-double exp_rapid_rec(double x, int n){
-    return 0;
+
+
+double exp_rapid_rec(double x, int n)
+{
+    double r = 0;
+
+    if(n == 0)
+        r = 1;
+    else if(n == 1)
+        r = x;
+    else if(n % 2 == 0)
+        r = exp_rapid_rec(x, n/2) * exp_rapid_rec(x, n/2);
+    else
+        r = x * exp_rapid_rec(x, n/2) * exp_rapid_rec(x, n/2);
+    
+    return r;
 }
+
 double exp_rapid_iter(double x, int n){
     double r=1;
     double a=x;//a temporaire
--- main.cpp
+++ main.cpp
@@ -18,13 +18,21 @@ int main(int argc, char *argv[]){
     clock_t end_r2 = clock();
     //Fin de calcul
     
+
+    //latency for exp_rapid_rec
+    clock_t begin_r3 = clock();
+    double r3 = exp_rapid_rec(x, n); 
+    clock_t end_r3 = clock();
+
     //convertir en ms
     unsigned long millis1 = (end_r1 -  begin_r1) * 1000 / CLOCKS_PER_SEC;
     unsigned long millis2 = (end_r2 -  begin_r2) * 1000 / CLOCKS_PER_SEC;
+    unsigned long millis3 = (end_r3 -  begin_r3) * 1000 / CLOCKS_PER_SEC;
             
     //print
     printf("exp_rapid_iter de %f^%d est : %f \n", x,n,r1);
     printf("exp_naif_rec %f^%d est : %f \n", x,n,r2);
-    
+    printf("exp_rapid_iter de %f^%d est : %f \n", x, n, r3);
+
     return 0;
 }
