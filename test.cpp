ef2cc80523fbf16460d865a858ca0ab43579dfb7
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2023-11-27 09:31:17 +0100

    build tests

---
+++ test.cpp
@@ -0,0 +1,14 @@
+// unit tests for 4 algorithms
+#include <iostream>
+#include <cstdint>
+#include <csetjmp>
+#include <cstdarg>
+#include <cstddef>
+
+#include <gtest/gtest.h>
+#include <catch2/catch.hpp> // to install ?
+
+//we include directly .cpp files to execute the tests
+#include "algo.cpp"
+#include "main.cpp"
+
