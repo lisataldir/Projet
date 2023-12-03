307c768ce57f41b2f1fe40e4f8c8cac326c152d7
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2023-12-03 19:24:27 +0100

    unit tests

--- test.cpp
+++ test.cpp
@@ -1,14 +1,43 @@
 // unit tests for 4 algorithms
-#include <iostream>
-#include <cstdint>
-#include <csetjmp>
-#include <cstdarg>
-#include <cstddef>
 
 #include <gtest/gtest.h>
-#include <catch2/catch.hpp> // to install ?
+
 
 //we include directly .cpp files to execute the tests
 #include "algo.cpp"
-#include "main.cpp"
 
+
+TEST(ExpNaifRec, BasicTest) 
+{
+    EXPECT_DOUBLE_EQ(exp_naif_rec(2.0, 0), 1.0);
+    EXPECT_DOUBLE_EQ(exp_naif_rec(2.0, 1), 2.0);
+    EXPECT_DOUBLE_EQ(exp_naif_rec(3.0, 3), 27.0);
+}
+
+TEST(ExpNaifIter, BasicTest) 
+{
+    EXPECT_DOUBLE_EQ(exp_naif_iter(2.0, 0), 1.0);
+    EXPECT_DOUBLE_EQ(exp_naif_iter(2.0, 1), 2.0);
+    EXPECT_DOUBLE_EQ(exp_naif_iter(3.0, 3), 27.0);
+}
+
+TEST(ExpRapidRec, BasicTest) 
+{
+    EXPECT_DOUBLE_EQ(exp_rapid_rec(2.0, 0), 1.0);
+    EXPECT_DOUBLE_EQ(exp_rapid_rec(2.0, 1), 2.0);
+    EXPECT_DOUBLE_EQ(exp_rapid_rec(3.0, 3), 27.0);
+}
+
+TEST(ExpRapidIter, BasicTest) 
+{
+    EXPECT_DOUBLE_EQ(exp_rapid_iter(2.0, 0), 1.0);
+    EXPECT_DOUBLE_EQ(exp_rapid_iter(2.0, 1), 2.0);
+    EXPECT_DOUBLE_EQ(exp_rapid_iter(3.0, 3), 27.0);
+}
+
+
+int main(int argc, char **argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
