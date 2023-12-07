0cb8fc5172949cd30a9223b317109ce9cf289b79
Lisa Taldir <lisa.taldir@free.fr>
2023-12-07 22:18:05 +0100

    Correct mistakes

--- exp_naif/build_UR.sh
+++ exp_naif/build_UR.sh
@@ -1,18 +1,18 @@
 #!bin/bash
 
-gcc sr.c -o sr
+gcc main.c -o main
 x=$(2)
 echo "# Creating folder results/UR_x_2.dat"
 ./reference "$x" >> results/UR_x_2.dat
-rm -rf sr.o sr
+rm -rf main.o main
 
-gcc sr.c -o sr
+gcc main.c -o main
 y=$(0.5)
 echo "# Creating folder results/UR_x_0_5.dat"
 ./reference "$y" >> results/UR_x_0_5.dat
 rm -rf sr.o sr
 
-gcc sr.c -o sr
+gcc main.c -o main
 z=$(1.34907566301)
 echo "# Creating folder results/UR_x_rand.dat"
 ./reference "$z" >> results/UR_x_rand.dat
