445eac5e3905418dbfefc3e540da132562e174f8
Lisa Taldir <lisa.taldir@free.fr>
2023-12-07 21:32:14 +0100

    Correct mistakes and rename build_arrondi_classique
    US = Usual Rounding
    Change the name output for SR and US

--- exp_naif/build_SR.sh
+++ exp_naif/build_SR.sh
@@ -6,23 +6,23 @@ mkdir results
 sudo docker pull verificarlo/verificarlo
 
 x=$(2)
-echo "# Creating folder output_x_$x.dat inside results"
-touch results/output_x_$x.dat
+echo "# Creating folder SR_x_$x.dat inside results"
+touch results/SR_x_$x.dat
 sudo docker run -v "$PWD":/workdir verificarlo/verificarlo verificarlo-c main.c -o main
-sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$x" >> results/output_x_$x.dat
+sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$x" >> results/SR_x_$x.dat
 rm -rf main.o main
 
 y=$(0.5)
-echo "# Creating folder output_x_$y.dat inside results"
-touch results/output_x_$y.dat
+echo "# Creating folder SR_x_$y.dat inside results"
+touch results/SR_x_$y.dat
 sudo docker run -v "$PWD":/workdir verificarlo/verificarlo verificarlo-c main.c -o main
-sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$y" >> results/output_x_$y.dat
+sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$y" >> results/SR_x_$y.dat
 rm -rf main.o main
 
 z=$(1.34907566301)
-echo "# Creating folder output_x_rand.dat inside results"
-touch results/output_x_rand.dat
+echo "# Creating folder SR_x_rand.dat inside results"
+touch results/SR_x_rand.dat
 sudo docker run -v "$PWD":/workdir verificarlo/verificarlo verificarlo-c main.c -o main
-sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$z" >> results/output_x_rad.dat
+sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$z" >> results/SR_x_rad.dat
 rm -rf main.o main
 
---
+++ exp_naif/build_UR.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+gcc sr.c -o sr
+x=$(2)
+echo "# Creating folder results/UR_x_$x.dat"
+./reference "$x" >> results/UR_x_$x.dat
+rm -rf sr.o sr
+
+gcc sr.c -o sr
+y=$(0.5)
+echo "# Creating folder results/UR_x_$y.dat"
+./reference "$y" >> results/UR_x_$y.dat
+rm -rf sr.o sr
+
+gcc sr.c -o sr
+z=$(1.34907566301)
+echo "# Creating folder results/UR_x_rand.dat"
+./reference "$z" >> results/UR_x_rand.dat
+rm -rf sr.o sr
