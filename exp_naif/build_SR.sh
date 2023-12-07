08fdfe8fda52a69c82045bfddd8da5b44cb0f52b
Lisa Taldir <lisa.taldir@free.fr>
2023-12-07 21:44:37 +0100

    Filenames change

--- exp_naif/build_SR.sh
+++ exp_naif/build_SR.sh
@@ -6,23 +6,22 @@ mkdir results
 sudo docker pull verificarlo/verificarlo
 
 x=$(2)
-echo "# Creating folder SR_x_$x.dat inside results"
-touch results/SR_x_$x.dat
+echo "# Creating folder SR_x_2.dat inside results"
+touch results/SR_x_2.dat
 sudo docker run -v "$PWD":/workdir verificarlo/verificarlo verificarlo-c main.c -o main
-sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$x" >> results/SR_x_$x.dat
+sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$x" >> results/SR_x_2.dat
 rm -rf main.o main
 
 y=$(0.5)
-echo "# Creating folder SR_x_$y.dat inside results"
-touch results/SR_x_$y.dat
+echo "# Creating folder SR_x_0_5.dat inside results"
+touch results/SR_x_0_5.dat
 sudo docker run -v "$PWD":/workdir verificarlo/verificarlo verificarlo-c main.c -o main
-sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$y" >> results/SR_x_$y.dat
+sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$y" >> results/SR_x_0_5.dat
 rm -rf main.o main
 
 z=$(1.34907566301)
 echo "# Creating folder SR_x_rand.dat inside results"
 touch results/SR_x_rand.dat
 sudo docker run -v "$PWD":/workdir verificarlo/verificarlo verificarlo-c main.c -o main
-sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$z" >> results/SR_x_rad.dat
+sudo docker run -v "$PWD":/workdir -e VFC_BACKENDS="libinterflop_mca.so" verificarlo/verificarlo ./main "$z" >> results/SR_x_rand.dat
 rm -rf main.o main
-
--- exp_naif/build_UR.sh
+++ exp_naif/build_UR.sh
@@ -2,14 +2,14 @@
 
 gcc sr.c -o sr
 x=$(2)
-echo "# Creating folder results/UR_x_$x.dat"
-./reference "$x" >> results/UR_x_$x.dat
+echo "# Creating folder results/UR_x_2.dat"
+./reference "$x" >> results/UR_x_2.dat
 rm -rf sr.o sr
 
 gcc sr.c -o sr
 y=$(0.5)
-echo "# Creating folder results/UR_x_$y.dat"
-./reference "$y" >> results/UR_x_$y.dat
+echo "# Creating folder results/UR_x_0_5.dat"
+./reference "$y" >> results/UR_x_0_5.dat
 rm -rf sr.o sr
 
 gcc sr.c -o sr
