a769dd48e4656a712a20f6d3537e7624fb5007d6
Lisa Taldir <lisa.taldir@free.fr>
2023-12-09 11:39:34 +0100

    Modification rapport

--- exp_naif/build_UR.sh
+++ exp_naif/build_UR.sh
@@ -1,19 +1,19 @@
 #!bin/bash
 
 gcc main.c -o main
-x=$(2)
+x=2
 echo "# Creating folder results/UR_x_2.dat"
-./reference "$x" >> results/UR_x_2.dat
+./main "$x" >> results/UR_x_2.dat
 rm -rf main.o main
 
 gcc main.c -o main
-y=$(0.5)
+y=0.5
 echo "# Creating folder results/UR_x_0_5.dat"
-./reference "$y" >> results/UR_x_0_5.dat
-rm -rf sr.o sr
+./main "$y" >> results/UR_x_0_5.dat
+rm -rf main.o main
 
 gcc main.c -o main
-z=$(1.34907566301)
+z=1.34907566301
 echo "# Creating folder results/UR_x_rand.dat"
-./reference "$z" >> results/UR_x_rand.dat
-rm -rf sr.o sr
+./main "$z" >> results/UR_x_rand.dat
+rm -rf main.o main
