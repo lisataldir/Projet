e6816a6a1e9cfa165140e8e7d9dfedefbe8b1912
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2023-12-11 01:25:25 +0100

    correction: x = 1.34907566301

--- exp_rapide/exp_rapide.h
+++ exp_rapide/exp_rapide.h
@@ -1,2 +1,2 @@
-float exp_rapide(int n, float x);
-float exp_naive(int n, float x);
+float exp_rapide_rec(int n, float x);
+float exp_rapide_iter(int n, float x);
--- exp_rapide/main.c
+++ exp_rapide/main.c
@@ -9,12 +9,10 @@
 #define EXP_RAPIDE_H
 #endif
 
-int n = 100000000;
-float x = 314.314;
+
 
 int main()
 {
-    float err = fabs(exp_rapide(n, x) - exp_naive(n, x));
-    printf("Error: %f\n", err);
+    
     return 0;
 }
