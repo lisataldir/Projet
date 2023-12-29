2256659e726de4f4620aa1ef973e8530b5376971
Lisa Taldir <lisa.taldir@free.fr>
2023-12-29 16:27:58 +0100

    Delete build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yangyizhi/Projet
-# Build directory: /Users/yangyizhi/Projet/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/yangyizhi/Projet/build/algo_test[1]_include.cmake")
-subdirs("algo_lib")
