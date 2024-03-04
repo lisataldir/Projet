0073d4edb42e25d69c0dbae6b4dcbbd5d5fee0af
Lisa Taldir <lisa.taldir@free.fr>
2024-03-04 17:35:16 +0100

    Results for 3 differents values of x

--- exponentiation/errSR.sh
+++ exponentiation/errSR.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 
+for k in {0..2}
+do
 # Numérotation
 awk '{
-    print (NR-1)*5;
-}' results/naif_rec/UR.dat > results/naif_rec/num.dat
-paste "results/naif_rec/num.dat" "results/naif_rec/UR.dat" > results/naif_rec/URstat.dat
-paste "results/naif_rec/num.dat" "results/naif_iter/UR.dat" > results/naif_iter/URstat.dat
-paste "results/naif_rec/num.dat" "results/rapide_rec/UR.dat" > results/rapide_rec/URstat.dat
-paste "results/naif_rec/num.dat" "results/rapide_iter/UR.dat" > results/rapide_iter/URstat.dat
+    print (NR-1)*10;
+}' results$k/naif_rec/UR.dat > results$k/naif_rec/num.dat
+paste "results$k/naif_rec/num.dat" "results$k/naif_rec/UR.dat" > results$k/naif_rec/URstat.dat
+paste "results$k/naif_rec/num.dat" "results$k/naif_iter/UR.dat" > results$k/naif_iter/URstat.dat
+paste "results$k/naif_rec/num.dat" "results$k/rapide_rec/UR.dat" > results$k/rapide_rec/URstat.dat
+paste "results$k/naif_rec/num.dat" "results$k/rapide_iter/UR.dat" > results$k/rapide_iter/URstat.dat
 
 # Regroupement de toutes nos données SR dans le même fichier
-paste $(for i in {1..50}; do echo -n "results/naif_rec/SR_$i.dat "; done) > results/naif_rec/SR.dat
+paste $(for i in {1..50}; do echo -n "results$k/naif_rec/SR_$i.dat "; done) > results$k/naif_rec/SR.dat
 # Calcul de la moyenne
 awk '{
     sum = 0;
@@ -18,41 +20,43 @@ awk '{
         sum += $i;
     }
     mean = sum / NF;
-    print (NR-1)*5, mean;
-}' results/naif_rec/SR.dat > results/naif_rec/SRstat.dat
-rm -rf results/naif_rec/SR_*.dat results/naif_rec/SR.dat results/naif_rec/UR.dat
+    print (NR-1)*10, mean;
+}' results$k/naif_rec/SR.dat > results$k/naif_rec/SRstat.dat
+rm -rf results$k/naif_rec/SR_*.dat results$k/naif_rec/SR.dat results$k/naif_rec/UR.dat
 
-paste $(for i in {1..50}; do echo -n "results/naif_iter/SR_$i.dat "; done) > results/naif_iter/SR.dat
+paste $(for i in {1..50}; do echo -n "results$k/naif_iter/SR_$i.dat "; done) > results$k/naif_iter/SR.dat
 awk '{
     sum = 0;
     for (i = 1; i <= NF; i++) {
         sum += $i;
     }
     mean = sum / NF;
-    print (NR-1)*5, mean;
-}' results/naif_iter/SR.dat > results/naif_iter/SRstat.dat
-rm -rf results/naif_iter/SR_*.dat results/naif_iter/SR.dat results/naif_iter/UR.dat
+    print (NR-1)*10, mean;
+}' results$k/naif_iter/SR.dat > results$k/naif_iter/SRstat.dat
+rm -rf results$k/naif_iter/SR_*.dat results$k/naif_iter/SR.dat results$k/naif_iter/UR.dat
 
-paste $(for i in {1..50}; do echo -n "results/rapide_rec/SR_$i.dat "; done) > results/rapide_rec/SR.dat
+paste $(for i in {1..50}; do echo -n "results$k/rapide_rec/SR_$i.dat "; done) > results$k/rapide_rec/SR.dat
 awk '{
     sum = 0;
     for (i = 1; i <= NF; i++) {
         sum += $i;
     }
     mean = sum / NF;
-    print (NR-1)*5, mean;
-}' results/rapide_rec/SR.dat > results/rapide_rec/SRstat.dat
-rm -rf results/rapide_rec/SR_*.dat results/rapide_rec/SR.dat results/rapide_rec/UR.dat
+    print (NR-1)*10, mean;
+}' results$k/rapide_rec/SR.dat > results$k/rapide_rec/SRstat.dat
+rm -rf results$k/rapide_rec/SR_*.dat results$k/rapide_rec/SR.dat results$k/rapide_rec/UR.dat
 
-paste $(for i in {1..50}; do echo -n "results/rapide_iter/SR_$i.dat "; done) > results/rapide_iter/SR.dat
+paste $(for i in {1..50}; do echo -n "results$k/rapide_iter/SR_$i.dat "; done) > results$k/rapide_iter/SR.dat
 awk '{
     sum = 0;
     for (i = 1; i <= NF; i++) {
         sum += $i;
     }
     mean = sum / NF;
-    print (NR-1)*5, mean;
-}' results/rapide_iter/SR.dat > results/rapide_iter/SRstat.dat
-rm -rf results/rapide_iter/SR_*.dat results/rapide_iter/SR.dat results/rapide_iter/UR.dat
+    print (NR-1)*10, mean;
+}' results$k/rapide_iter/SR.dat > results$k/rapide_iter/SRstat.dat
+rm -rf results$k/rapide_iter/SR_*.dat results$k/rapide_iter/SR.dat results$k/rapide_iter/UR.dat
 
-rm -rf results/naif_rec/num.dat
+rm -rf results$k/naif_rec/num.dat
+
+done
---
+++ exponentiation/plot2/plot.gp
@@ -0,0 +1,43 @@
+set terminal png   
+set output 'plot_ni.png'  
+
+set size square
+set grid
+set xlabel 'Puissance (n)'        
+set ylabel 'Erreur relative' 
+
+plot "../results2/naif_iter/URstat.dat" with points pointsize 1.2 title "NR", \
+     "../results2/naif_iter/SRstat.dat" with points pointsize 1.2 title "SR"
+
+set terminal png   
+set output 'plot_nr.png'  
+
+set size square
+set grid
+set xlabel 'Puissance (n)'        
+set ylabel 'Erreur relative' 
+
+plot "../results2/naif_rec/URstat.dat" with points pointsize 1.2 title "NR", \
+     "../results2/naif_rec/SRstat.dat" with points pointsize 1.2 title "SR"
+
+set terminal png   
+set output 'plot_ri.png'  
+
+set size square
+set grid
+set xlabel 'Puissance (n)'        
+set ylabel 'Erreur relative'  
+
+plot "../results2/rapide_iter/URstat.dat" with points pointsize 1.2 title "NR", \
+     "../results2/rapide_iter/SRstat.dat" with points pointsize 1.2 title "SR"
+
+set terminal png   
+set output 'plot_rr.png'  
+
+set size square
+set grid
+set xlabel 'Puissance (n)'        
+set ylabel 'Erreur relative'  
+
+plot "../results2/rapide_rec/URstat.dat" with points pointsize 1.2 title "NR", \
+     "../results2/rapide_rec/SRstat.dat" with points pointsize 1.2 title "SR"
