33f4f46c026a5a8aaf1a1379a091d71c8f09ec87
“Yutai <yutai.zhao@gmail.com>
2024-03-13 22:23:58 +0100

    "Scripts for elaps done" - timing raw data and elaps calcul are sperated :  -> cuz timing file will execute with SR  -> To prevent that SR applying on elaps calcul - tested with UR, to be tested with SR (code to be modified in run.sh) - next step is to plot the elaps

--- Performance SR/run.sh
+++ Performance SR/run.sh
@@ -7,13 +7,22 @@ mkdir -p timing_SR/ timing_UR/
 
 for i in {1..20}
 do
+###Enlever ces 2 lignes et activer les 4 suivantes 
 echo "# Testing"
 python3 performance.py >> timing_UR/raw_res.dat
 
-echo "# Creating folder timing_UR/raw_res.dat"
+#echo "# Creating folder timing_UR/raw_res.dat"
 #NEURON_RT_STOCHASTIC_ROUNDING_EN=0 python3 preformance.py >> timing_UR/raw_res.dat #UR
-echo "# Creating folder timing_SR/raw_res.dat"
+#echo "# Creating folder timing_SR/raw_res.dat"
 #NEURON_RT_STOCHASTIC_ROUNDING_EN=1 python3 preformance.py >> timing_SR/raw_res.dat #SR
 done
 
+for mode in "timing_UR" "timing_SR"
+do
+for algo in "naif" "rapid"
+do
+grep "$algo" "$mode"/raw_res.dat >> "$mode"/raw_"$algo".dat
+done
+done
 
+python3 traitement_donnees.py
