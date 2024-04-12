5f79eef4321739d2a12a934291734d63babf3b27
“Yutai <yutai.zhao@gmail.com>
2024-04-12 19:51:04 +0200

    perf files organization & data prepared

---
+++ Performance SR/timing_version1_1/time.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#nb_iter=500
+
+cat timing_SR/raw_res.dat | grep sum >> timing_SR/raw_sum.dat
+cat timing_SR/raw_res.dat | grep naif >> timing_SR/raw_naif.dat
+cat timing_SR/raw_res.dat | grep rapid >> timing_SR/raw_rapid.dat
+
+cat timing_UR/raw_res.dat | grep sum >> timing_UR/raw_sum.dat
+cat timing_UR/raw_res.dat | grep naif >> timing_UR/raw_naif.dat
+cat timing_UR/raw_res.dat | grep rapid >> timing_UR/raw_rapid.dat
+
+python3 traitement_donnees.py 500
+
