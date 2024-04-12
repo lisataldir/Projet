5eec3ec0c6e4571415f2d98a245da76079aff10e
“Yutai <yutai.zhao@gmail.com>
2024-04-12 13:15:28 +0200

    final version of SRperf

--- Performance SR/performance.py
+++ Performance SR/performance.py
@@ -4,21 +4,22 @@ import torch
 import sys
 import torch_xla
 import torch_xla.core.xla_model as xm
+import random
 device = xm.xla_device()
 
-a = torch.tensor(2, dtype=torch.float64).to(device)
-n = 100
+a = torch.tensor(random.uniform(1.01, 1.19)).half().to(device)
+n = 60
 nb_iter = int(sys.argv[1])
 
 def somme(x,n):
-    r =  torch.tensor(0.0, dtype=torch.float64).to(device)
+    r =  torch.tensor(0.0).half().to(device)
     for i in range(n):
         r += x
         xm.mark_step()
     return r
 
 def exp_naif(x,n):
-    r =  torch.tensor(1.0, dtype=torch.float64).to(device)
+    r =  torch.tensor(1.0).half().to(device)
     for i in range(n):
         r *= x
         xm.mark_step()
@@ -26,7 +27,7 @@ def exp_naif(x,n):
 
 
 def exp_rapid(x, n):
-    r =  torch.tensor(1.0, dtype=torch.float64).to(device)
+    r =  torch.tensor(1.0).half().to(device)
     while n != 0:
         if n % 2 == 1:
             r *= x
@@ -36,27 +37,23 @@ def exp_rapid(x, n):
     return r
 
 
-r_sum = 0.0
+r_sum = torch.tensor(0.0).half().to(device)
 start = time.time()
 for i in range(nb_iter):
     r_sum = somme(a.clone(),n)
 end = time.time()
-print(f"naif\t{r_sum}\t{start:.17f}\t{end:.17f}") 
+print(f"sum\t{a}*{n}\t{start:.17f}\t{end:.17f}")
 
-r_naif = 0.0
+r_naif = torch.tensor(0.0).half().to(device)
 start = time.time()
 for i in range(nb_iter):
     r_naif = exp_naif(a.clone(),n)
 end = time.time()
-print(f"naif\t{r_naif}\t{start:.17f}\t{end:.17f}") 
+print(f"naif\t{a}^{n}\t{start:.17f}\t{end:.17f}")
 
-r_rapid = 0.0
+r_rapid = torch.tensor(0.0).half().to(device)
 start = time.time()
 for i in range(nb_iter):
     r_rapid = exp_rapid(a.clone(),n)
 end = time.time()
-print(f"rapid\t{r_rapid}\t{start:.17f}\t{end:.17f}")
-
-
-
-
+print(f"rapid\t{a}^{n}\t{start:.17f}\t{end:.17f}")
--- Performance SR/run.sh
+++ Performance SR/run.sh
@@ -7,19 +7,11 @@ mkdir -p timing_SR/ timing_UR/
 
 #nb_iter=1000
 
-for i in {1..33}
+for i in {1..100}
 do
-echo "$i-ieme experience de 1000 repetitions de calculs"
-NEURON_RT_STOCHASTIC_ROUNDING_EN=0 python3 performance.py 1000 >> timing_UR/raw_res.dat #UR
+echo "$i-ieme experience avec 1000 repetitions de calculs"
+echo "RNE..."
+NEURON_RT_STOCHASTIC_ROUNDING_EN=0 python3 performance.py 1000 >> timing_UR/raw_res.dat #RNE
+echo "SR..."
 NEURON_RT_STOCHASTIC_ROUNDING_EN=1 python3 performance.py 1000 >> timing_SR/raw_res.dat #SR
 done
-
-for mode in "timing_UR" "timing_SR"
-do
-for algo in "naif" "rapid"
-do
-grep "$algo" "$mode"/raw_res.dat >> "$mode"/raw_"$algo".dat
-done
-done
-
-python3 traitement_donnees.py 1000
