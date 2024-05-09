df4c9663ad94e59fab5468974c333749ff307fe9
LisaTaldir <lisa.taldir@free.fr>
2024-05-09 10:16:08 +0200

    Rename files

--- multiplication/algo.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-double ref_multi(double x, int n);
-
-float multi_ni(float x, int n);
--- sum_prod/algo.h
+++
@@ -1,2 +0,0 @@
-double ref_sum_prod(double *a, int vec_nb, int vec_size);
-float sum_prod(float *a, int vec_nb, int vec_size);
